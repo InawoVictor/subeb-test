77ecd367b0ff9ee39d1e34dab7ec6ba42cefee39
InawoVictor <inawovictor004@gmail.com>
2024-02-29 02:28:50 +0100

    Added alll the about pages

---
+++ components/About/History.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container mt-12 py-8">
+        <div class="flex justify-between gap-16 items-end ">
+            <div class="p-4 ">
+                <img src="/img/subeb-building.png" class="w-[492px] h-[453px]" alt="">
+            </div>
+            <div class="flex flex-col justify-end gap-y-16">
+                <h2 class="font-bold font-dmSans text-primary text-[50px]">Our History</h2>
+                <p class="font-normal font-dmSans text-[20px] leading-[30px] ">
+                    he Cross River State Universal Basic Education Board (SUBEB)
+                    was founded in compliance with the Public Procurement Act of 2007,
+                    aligning with Nigeria's national directive to establish
+                    SUBEBs across the country. This legislation marked a pivotal
+                    moment in educational governance, formalizing the role of SUBEBs i
+                    n overseeing the implementation of the Universal Basic Education (UBE) program within each state. As part of this framework, the Cross River State SUBEB was established to coordinate and supervise primary and junior secondary school education initiatives, including the construction and renovation of schools, teacher recruitment and training,
+                    curriculum development, and educational standards monitoring.
+                </p>
+            </div>
+        </div>
+        <p class="mt-8 font-normal font-dmSans text-[20px] leading-[30px]">
+            he Cross River State Universal Basic Education Board 
+            (SUBEB) was founded in compliance with the Public Procurement 
+            Act of 2007, aligning with Nigeria's national directive to 
+            establish SUBEBs across the country. This legislation 
+            marked a pivotal moment in educational governance, formalizing 
+            the role of SUBEBs in overseeing the implementation of 
+            the Universal Basic Education (UBE) program within each 
+            state. As part of this framework, the Cross River State 
+            SUBEB was established to coordinate and supervise primary             
+            and junior secondary school education initiatives, 
+            including the construction and renovation of schools, teacher recruitment 
+            and training, curriculum development, and educational standards monitoring.
+        </p>
+    </div>
+</template>
---
+++ pages/about/crsubeb.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-grayBg">
+        <ResourcesHero title1="About" title2="CRSUBEB" />
+        <HomeIntro :isButton="false"/>
+        <AboutHistory />
+        <AboutVision />
+        <HomeObjective :isButton="false"/>
+    </div>
+</template>
